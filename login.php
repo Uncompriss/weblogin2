b92170df56b6b3fc22e4a40c2aa68e06c3829d1e
alexfloay89 <a.flores.aybar@gmail.com>
2023-06-04 14:52:37 -0500

    preuba
    prueba

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+// Conectarse a la base de datos
+$servername = "localhost";
+$username = "usuario";
+$password = "contraseña";
+$dbname = "nombre_basedatos";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener los datos del formulario
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Consulta para verificar los datos de inicio de sesión
+$sql = "SELECT * FROM usuarios WHERE username='$username' AND password='$password'";
+$result = $conn->query($sql);
+
+// Verificar si los datos son correctos
+if ($result->num_rows > 0) {
+    echo "Bienvenido, has iniciado sesión correctamente.";
+} else {
+    echo "Datos de inicio de sesión inválidos.";
+}
+
+// Cerrar la conexión a la base de datos
+$conn->close();
+?>
