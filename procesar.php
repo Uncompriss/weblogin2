88b1f1f0bedd41862a7ecf7d4735f9a586900766
alexfloay89 <a.flores.aybar@gmail.com>
2023-06-04 17:38:16 -0500

    t8
    t8

--- procesar.php
+++ procesar.php
@@ -1,7 +1,3 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $mensaje = $_POST["mensaje"];
-  echo "<h2>El mensaje es: </h2>";
-  echo "<textarea rows='4' cols='50'>$mensaje</textarea>";
-}
+echo"hola mundo";
 ?>
